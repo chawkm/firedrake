23b970fe5070949936d0f0c2b1af870570616005
Christopher Hawkes <ch3915@ic.ac.uk>
2016-07-12 14:38:25 +0100

    Initial Commit
    Produces sine wave in a periodic mesh, the coulours become lighter due
    to the innacuracy. This is hoped to be fixed with slope limiters.

---
+++ tests/limiters/advection.py
@@ -0,0 +1,78 @@
+from firedrake import *
+import numpy as np
+
+
+def run_test(mesh, outfile):
+#    mesh.init_cell_orientations(Expression(("x[0]", "x[1]", "x[2]")))
+
+    V = FunctionSpace(mesh, "DG", 0)
+    M = VectorFunctionSpace(mesh, "CG", 1)
+
+    # advecting velocity
+    u0 = Expression(('1','0'))
+    u = Function(M).interpolate(u0)
+    
+    dt = (pi/3) * 0.006
+
+    phi = TestFunction(V)
+    D = TrialFunction(V)
+
+    n = FacetNormal(mesh)
+
+    un = 0.5 * (dot(u, n) + abs(dot(u, n))) #upwind value
+
+    a_mass = phi*D*dx
+    a_int = dot(grad(phi), -u*D)*dx
+    a_flux = dot(jump(phi), un('+')*D('+') - un('-')*D('-'))*dS
+    arhs = a_mass - dt * (a_int + a_flux)
+
+    dD1 = Function(V)
+    D1 = Function(V)
+
+    #changed
+    x = SpatialCoordinate(mesh)
+    D0 = Expression(sin(2*pi*x[0]))
+    D = Function(V).interpolate(D0)
+    D_old = Function(D)
+    outfile.write(D1)
+
+    iterations = 100
+    t = 0.0
+    T = iterations*dt
+
+    problem = LinearVariationalProblem(a_mass, action(arhs, D1), dD1)
+    solver = LinearVariationalSolver(problem, solver_parameters={'ksp_type': 'cg'})
+
+    L2_0 = norm(D)
+    Dbar_0 = assemble(D*dx)
+    while t < (T - dt/2):
+        D1.assign(D)
+        solver.solve()
+        D1.assign(dD1)
+
+        solver.solve()
+        D1.assign(0.75*D + 0.25*dD1)
+        solver.solve()
+        D.assign((1.0/3.0)*D + (2.0/3.0)*dD1)
+
+        t += dt
+        outfile.write(D1, time=t)
+
+    L2_T = norm(D)
+    Dbar_T = assemble(D*dx)
+
+    # L2 norm decreases
+    #assert L2_T < L2_0
+
+    # Mass conserved
+    #assert np.allclose(Dbar_T, Dbar_0)
+
+
+if __name__ == '__main__':
+    print "**********Start**********"
+    run_test(PeriodicUnitSquareMesh(30,30), File("Periodic.pvd"))
+    '''
+    run_test(UnitIcosahedralSphereMesh(refinement_level=3), File("UnitIcs.pvd"))
+    run_test(UnitCubedSphereMesh(refinement_level=4),File("UnitCbd.pvd"))
+    '''
+    print "**********Done**********"
